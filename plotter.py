f8fefac80b6d1901d1763c8a61288fc3ef9b8b44
Maxwell Nye <maxnye@comcast.net>
2018-02-10 16:42:20 -0500

    added plotter.py

---
+++ plotter.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 12:05:09 2017
+
+@author: Maxwell
+
+Run averaging code:
+    
+    Designed to work with the odessey code "launch_fouriernetwork."
+    
+    Assuming all the runs in an experiment have been done with the same hyperparameters, 
+    this code will average the desired parameters
+"""
+
+import glob
+import numpy as np
+import argparse
+from hand_code_real_fft_network_odyssey import hand_code_real_fft_network_fun
+from hand_code_real_fft_network_odyssey import hand_code_fun_layer_less
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-expt', type=int)
+parser.add_argument('-size', type=int)
+
+settings = parser.parse_args(); 
+
+experiment_num = settings.expt
+size = settings.size
+
+basepath = "/n/home09/mnye/parity_fn/results/expt%d/" % experiment_num
+directory_path = basepath + 'data/'
+plotpath = basepath + 'plots/'
+
+fun_loss_list = []
+weightscale_list = []
+for res_num in glob.glob(directory_path + '*.npz'):
+    try:
+        variables = np.load(res_num)
+        run_params = variables['params']    
+        
+        if run_params.size == size:
+            fun_loss_list.append(variables['fnloss'])
+            weightscale_list.append(run_params.weightscale)
+
+    except IOError:
+        print("there exists a trial which is not complete")
+
+        #Whatever man    
+assert len(fun_loss_list) == len(weightscle_list)
+
+fig = plt.figure()
+fig, ax = plt.subplots()
+plt.plot(y,x)
+ax.set(title='Parity function convergence',
+       xlabel='Weight initialization noise scale',
+       ylabel='Final network error')
+
+fig.savefig(plotpath + "paritysize%.png" % size, dpi=200)
+
+
+ 
